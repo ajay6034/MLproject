3dc1fb40602147e3d3cff79789aaba2c3c849f74
AJAY KUMAR JAGU <ajaykumarjagu155@gmail.com>
2024-07-11 11:08:36 -0400

    project structure

--- src/logger.py
+++ src/logger.py
@@ -3,13 +3,14 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path =os.path.join(os.getcd(),"logs",LOG_FILE)
-os.maedirs(log_path,exist_ok=True)
+log_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_path, exist_ok=True)
 
-LOG_FILE_PATH =os.path.join(log_path,LOG_FILE)
+LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE,
-    format="[%(asctime)s] %(lineno) %(name)s - %(levelname)s- %(message)s",
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+
